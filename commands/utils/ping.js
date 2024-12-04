a9252f0915dac291585cb7e78f5db75c3de80f11
nulxn <oioioihomelander@yahoo.com>
2024-12-04 11:37:18 -0800

    register ping

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,10 +1,25 @@
 import { SlashCommandBuilder } from "discord.js";
+import { exec } from "child_process";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    await interaction.reply("Pong! Registering commands...");
+
+    exec("node ~/athletic-bot/register.js", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+
+      console.log(`stdout: ${stdout}`);
+    });
   },
 };
