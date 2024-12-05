065dca2ec26cfb7ce2b46847263e35317cfa129e
nulxn <oioioihomelander@yahoo.com>
2024-12-04 16:48:31 -0800

    test

--- commands/fantasy/register.js
+++ commands/fantasy/register.js
@@ -4,14 +4,18 @@ import { createUser, db } from "../../db.js";
 export default {
   data: new SlashCommandBuilder()
     .setName("register")
-    .setDescription("Register for the fantasy league"),
+    .setDescription("Register for the fantasy league")
+    .addStringOption((option) =>
+      option.setName("name").setDescription("Your name").setRequired(true)
+    ),
   async execute(interaction) {
     const user = interaction.user;
-    const users = await db.collection("users").get();
-    const userExists = users.docs.some((doc) => doc.data().id === user.id);
+    const name = interaction.options.getString("name");
+    const users = await db.query("SELECT * FROM users");
+    const userExists = true;
 
     if (userExists) {
-      await interaction.reply("You are already registered.");
+      await interaction.reply(JSON.stringify(users));
     } else {
       await createUser(user.id);
       await interaction.reply("You have been registered successfully.");
