04b38b61ac0fa555480419ebc0f93c7219eb0c03
nulxn <oioioihomelander@yahoo.com>
2025-01-04 18:01:15 -0800

    Small Fixes

--- commands/utils/info.js
+++ commands/utils/info.js
@@ -1,10 +1,14 @@
 import { SlashCommandBuilder } from "discord.js";
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName("info")
-        .setDescription("Replies with information about the bot."),
-    async execute(interaction) {
-        await interaction.reply(`Bot uptime: ${process.uptime()} seconds\nBot latency: ${-(Date.now() - interaction.createdTimestamp)}ms\nAPI latency: ${interaction.client.ws.ping}ms`);
-    }
+  data: new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Replies with information about the bot."),
+  async execute(interaction) {
+    await interaction.reply(
+      `Bot uptime: ${process.uptime()} seconds\nBot latency: ${-(
+        Date.now() - interaction.createdTimestamp
+      )}ms\nAPI latency: ${interaction.client.ws.ping}ms`
+    );
+  },
 };
